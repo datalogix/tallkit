6af728d31b4cfe6fd803fcdaba923eb3ec148966
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2026-01-09 17:00:35 -0300

    Build assets

--- dist/tallkit.js
+++ dist/tallkit.js
@@ -2198,8 +2198,8 @@
     if (typeof args[0] === "object" && args[0] !== null && !Array.isArray(args[0])) {
       detail = args[0];
     } else {
-      const [text, heading, type, duration, position] = args;
-      detail = { text, heading, type, duration, position };
+      const [message, title, type, duration, position] = args;
+      detail = { message, title, type, duration, position };
     }
     document.dispatchEvent(new CustomEvent("toast", { detail }));
   }
